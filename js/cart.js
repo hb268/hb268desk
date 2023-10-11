b982f7622e1f36b8253933d095c16c5a24b3e160
Facundo Garcia <141085320+FacundoGarcia28@users.noreply.github.com>
2023-10-11 20:18:40 -0300

    Update cart.js

--- js/cart.js
+++ js/cart.js
@@ -4,23 +4,33 @@ const apiUrl = `https://japceibal.github.io/emercado-api/user_cart/${userId}.jso
 const tablaProductosBody = document.getElementById("tablaProductosBody");
 const newRow = document.createElement("tr")
 
+
 addEventListener("DOMContentLoaded", () => { 
     fetch(apiUrl)
     .then(response => response.json())
     .then(data => {
         const articulosDisponibles = data.articles[0];
 
-;
-newRow.innerHTML += `
-                    <td class="col-lg-2"><img src="${articulosDisponibles.image}" alt="${articulosDisponibles.name}" class="img-thumbnail"></td>
-                    <td>${articulosDisponibles.name}</td>
-                    <td>${articulosDisponibles.currency} ${articulosDisponibles.unitCost}</td>
-                    <td><input type="number" min="1" max="${articulosDisponibles.count}" value="1" id="cantidad_${articulosDisponibles.id}" class="form-control"></td>
-                    <td id="total_${articulosDisponibles.id}"><b>${articulosDisponibles.currency} ${articulosDisponibles.unitCost}</b></td>
-                       `;
-                        tablaProductosBody.appendChild(newRow);
-                            })
+        const newRow = document.createElement("tr");
+        newRow.innerHTML = `
+            <td class= "h-25" style="width: 100px"><img src="${articulosDisponibles.image}" alt="${articulosDisponibles.name}" class="img-thumbnail"></td>
+            <td>${articulosDisponibles.name}</td>
+            <td>${articulosDisponibles.currency} ${articulosDisponibles.unitCost}</td>
+            <td><input type="number" min="1" max="${articulosDisponibles.count}" value="1" id="cantidad_${articulosDisponibles.id}" class="form-control"></td>
+            <td id="total_${articulosDisponibles.id}"><b>${articulosDisponibles.currency} ${articulosDisponibles.unitCost}</b></td>
+        `;
+        tablaProductosBody.appendChild(newRow);
+
+        // Agregar un evento de escucha al campo de entrada de la cantidad
+        document.getElementById(`cantidad_${articulosDisponibles.id}`).addEventListener('input', (event) => {
+            const cantidad = event.target.value;
+            const subtotal = articulosDisponibles.unitCost * cantidad;
+            document.getElementById(`total_${articulosDisponibles.id}`).innerHTML = `<b>${articulosDisponibles.currency} ${subtotal}</b>`;
+        });
+    })
 });
+
+;
 
 addEventListener("DOMContentLoaded", AgregarACarrito)
 
