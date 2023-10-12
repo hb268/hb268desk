1b1d50e8c20a8910373ef8eec6da2306cd64b938
vainntn <valentin.acosta512@gmail.com>
2023-10-11 21:01:53 -0300

    entrega5_casifinal

--- js/cart.js
+++ js/cart.js
@@ -2,19 +2,19 @@ var userId = 25801;
 const apiUrl = `https://japceibal.github.io/emercado-api/user_cart/${userId}.json`;
 
 const tablaProductosBody = document.getElementById("tablaProductosBody");
-addEventListener("DOMContentLoaded", () => { 
+addEventListener("DOMContentLoaded", () => {
 
     fetch(apiUrl)
-    .then(response => response.json())
-    .then(data => {
-    const articles = data.articles;
+        .then(response => response.json())
+        .then(data => {
+            const articles = data.articles;
 
 
-    articles.forEach(articulo => {
-        const newRow = document.createElement("tr");
-        newRow.setAttribute("data-id", articulo.id);
+            articles.forEach(articulo => {
+                const newRow = document.createElement("tr");
+                newRow.setAttribute("data-id", articulo.id);
 
-        newRow.innerHTML = `
+                newRow.innerHTML = `
         <td class="h-25" style="width: 100px"><img src="${articulo.image}" alt="${articulo.name}" class="img-thumbnail"></td>
         <td>${articulo.name}</td>
         <td>${articulo.currency} ${articulo.unitCost}</td>
@@ -22,43 +22,19 @@ addEventListener("DOMContentLoaded", () => {
         <td id="total_${articulo.id}"><b>${articulo.currency} ${articulo.unitCost}</b></td>
         `;
 
-        tablaProductosBody.appendChild(newRow);
+                tablaProductosBody.appendChild(newRow);
 
-        
-        document.getElementById(`cantidad_${articulo.id}`).addEventListener('input', (event) => {
-        const cantidad = event.target.value;
-          const subtotal = articulo.unitCost * cantidad;
-        document.getElementById(`total_${articulo.id}`).innerHTML = `<b>${articulo.currency} ${subtotal}</b>`;
+
+                document.getElementById(`cantidad_${articulo.id}`).addEventListener('input', (event) => {
+                    const cantidad = event.target.value;
+                    const subtotal = articulo.unitCost * cantidad;
+                    document.getElementById(`total_${articulo.id}`).innerHTML = `<b>${articulo.currency} ${subtotal}</b>`;
+                });
+            });
         });
-    });
 });
-});
-
-
-addEventListener("DOMContentLoaded", AgregarACarrito);
-
-        const newRow2 = document.createElement("tr");
-        newRow2.innerHTML = `
-            <td class= "h-25" style="width: 100px"><img src="${articulosDisponibles.image}" alt="${articulosDisponibles.name}" class="img-thumbnail"></td>
-            <td>${articulosDisponibles.name}</td>
-            <td>${articulosDisponibles.currency} ${articulosDisponibles.unitCost}</td>
-            <td><input type="number" min="1" max="${articulosDisponibles.count}" value="1" id="cantidad_${articulosDisponibles.id}" class="form-control"></td>
-            <td id="total_${articulosDisponibles.id}"><b>${articulosDisponibles.currency} ${articulosDisponibles.unitCost}</b></td>
-        `;
-        tablaProductosBody.appendChild(newRow);
-
-        document.getElementById(`cantidad_${articulosDisponibles.id}`).addEventListener('input', (event) => {
-            const cantidad = event.target.value;
-            const subtotal = articulosDisponibles.unitCost * cantidad;
-            document.getElementById(`total_${articulosDisponibles.id}`).innerHTML = `<b>${articulosDisponibles.currency} ${subtotal}</b>`;
-        });
-
-
-
-
 
 addEventListener("DOMContentLoaded", AgregarACarrito)
-
 
 function AgregarACarrito() {
     const carrito = JSON.parse(localStorage.getItem('cart')) || [];
@@ -66,17 +42,6 @@ function AgregarACarrito() {
     tablaProductosBody.innerHTML = '';
 
     carrito.forEach(element => {
-
-        Row.innerHTML += `
-    <td class= "h-25" style="width: 100px"><img src="${element.images[0]}" alt="" class="img-thumbnail"></td>
-    <td>${element.name}</td>
-    <td>${element.currency} ${element.cost}</td>
-    <td><input type="number" min="1" max="$100" value="1" id="cantidad_${element.id}" class="form-control"></td>
-    <td id="total_${element.id}"><b>${element.currency} ${element.cost}</b></td>
-    `;
-    tablaProductosBody.appendChild(Row)
-    })
-};
 
         const newRow = document.createElement("tr");
         newRow.innerHTML = `
@@ -87,7 +52,11 @@ function AgregarACarrito() {
             <td id="total_${element.id}"><b>${element.currency} ${element.cost}</b></td>
         `;
         tablaProductosBody.appendChild(newRow);
-
+        document.getElementById(`cantidad_${element.id}`).addEventListener('input', (event) => {
+            const cantidad = event.target.value;
+            const subtotal = element.cost * cantidad;
+            document.getElementById(`total_${element.id}`).innerHTML = `<b>${element.currency} ${subtotal}</b>`;
+        });
+    })
+};
 AgregarACarrito();
-
-
