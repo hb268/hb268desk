0e3cdfeffb7643fee86cddd5fb15cb1048cba3d9
vainntn <valentin.acosta512@gmail.com>
2023-10-25 14:49:10 -0300

    p1.1

--- js/cart.js
+++ js/cart.js
@@ -17,8 +17,8 @@ document.addEventListener("DOMContentLoaded", () => {
                 <td class="image-container"><img src="${articulo.image}" alt="${articulo.name}" class="img-thumbnail"></td>
                 <td>${articulo.name}</td>
                 <td>${articulo.currency} ${articulo.unitCost}</td>
-                <td><input type="number" min="1" max="9999" value="1" id="cantidad_${articulo.id}" class="form-control"></td>
-                <td id="total_${articulo.id}"><b>${articulo.currency} ${articulo.unitCost}</b></td>
+                <td><input type="number" min="1" max="9999" value="1" id="cantidad_${articulo.id}" class="form-control" onchange="recalcularCostos()"></td>
+                <td id="total_${articulo.id}"><p>${articulo.currency} ${articulo.unitCost}</p></td>
                 <td><button class="btn btn-danger" id="eliminarPrecargado">Eliminar</button></td>
             `;
 
@@ -27,12 +27,15 @@ document.addEventListener("DOMContentLoaded", () => {
                 const eliminarPrecargadoButton = newRow.querySelector('#eliminarPrecargado');
                 eliminarPrecargadoButton.addEventListener('click', () => {
                     newRow.style.display = 'none';
+                    const cantidadInput = document.getElementById(`cantidad_${articulo.id}`);
+                    const cantidad = parseInt(cantidadInput.value);
+                    recalcularCostos(articulo.unitCost * cantidad);
                 });
 
                 document.getElementById(`cantidad_${articulo.id}`).addEventListener('input', (event) => {
                     const cantidad = event.target.value;
                     const subtotal = articulo.unitCost * cantidad;
-                    document.getElementById(`total_${articulo.id}`).innerHTML = `<b>${articulo.currency} ${subtotal}</b>`;
+                    document.getElementById(`total_${articulo.id}`).innerHTML = `<p>${articulo.currency} ${subtotal}</p>`;
                 });
             });
         });
@@ -51,8 +54,8 @@ function AgregarACarrito() {
             <td class="image-container"><img src="${element.images[0]}" alt="" class="img-thumbnail img-fluid"></td>
             <td>${element.name}</td>
             <td>${element.currency} ${element.cost}</td>
-            <td><input type="number" min="1" max="100" value="1" id="cantidad_${element.id}" class="form-control"></td>
-            <td id="total_${element.id}"><b>${element.currency} ${element.cost}</b></td>
+            <td><input type="number" min="1" max="100" value="1" id="cantidad_${element.id}" class="form-control" onchange="recalcularCostos()"></td>
+            <td id="total_${element.id}"><p>${element.currency} ${element.cost}</p></td>
             <td><button class="btn btn-danger" id="eliminar_${element.id}">Eliminar</button></td>
         `;
         tablaProductosBody.appendChild(newRow);
@@ -63,18 +66,88 @@ function AgregarACarrito() {
         cantidadInput.addEventListener('input', (event) => {
             const cantidad = event.target.value;
             const subtotal = element.cost * cantidad;
-            document.getElementById(`total_${element.id}`).innerHTML = `<b>${element.currency} ${subtotal}</b>`;
+            document.getElementById(`total_${element.id}`).innerHTML = `<p>${element.currency} ${subtotal}</p>`;
         });
 
         eliminarButton.addEventListener('click', () => {
+            const cantidadInput = document.getElementById(`cantidad_${element.id}`);
+            const cantidad = parseInt(cantidadInput.value);
+            const subtotal = element.cost * cantidad;
             carrito.splice(carrito.findIndex(item => item.id === element.id), 1);
             localStorage.setItem('cart', JSON.stringify(carrito));
-            window.location.href = 'cart.html';
+            const rowToDelete = document.getElementById(`total_${element.id}`).closest('tr');
+            rowToDelete.style.display = 'none';
+            recalcularCostos(subtotal);
         });
     });
 }
 
 AgregarACarrito();
+
+document.addEventListener("DOMContentLoaded", () => {
+    recalcularCostos();
+});
+
+const subtotalValue = document.getElementById("importeSubtotal");
+const envioValue = document.getElementById("importeEnvio");
+const totalValue = document.getElementById("importeTotal");
+const radioButtons = document.querySelectorAll('input[name="inlineRadioOptions"]');
+const carrito = JSON.parse(localStorage.getItem('cart')) || [];
+
+function recalcularCostos(descuento) {
+    let subtotal = 0;
+    if (descuento > 0) {
+        subtotal -= descuento;
+    }
+    carrito.forEach((element) => {
+        const cantidadInput = document.getElementById(`cantidad_${element.id}`);
+        const precioProducto = parseFloat(element.cost);
+        const cantidad = parseInt(cantidadInput.value);
+        const subtotalProducto = precioProducto * cantidad;
+
+        subtotal += subtotalProducto;
+    });
+    const apiUrl = `https://japceibal.github.io/emercado-api/user_cart/${userId}.json`;
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            const articles = data.articles;
+            articles.forEach(articulo => {
+                const unitCost = parseFloat(articulo.unitCost);
+                const cantidadInput = document.getElementById(`cantidad_${articulo.id}`);
+                const cantidad = parseInt(cantidadInput.value);
+                const subtotalProducto = unitCost * cantidad;
+                subtotal += subtotalProducto;
+            });
+
+            let costoEnvio = 0;
+            radioButtons.forEach((radio) => {
+                if (radio.checked) {
+                    const selectedValue = radio.value;
+                    if (selectedValue === "premium") {
+                        costoEnvio = subtotal * 0.15;
+                    } else if (selectedValue === "express") {
+                        costoEnvio = subtotal * 0.07;
+                    } else if (selectedValue === "Estándar") {
+                        costoEnvio = subtotal * 0.05;
+                    }
+                }
+            });
+
+            subtotalValue.textContent = subtotal.toFixed(2);
+            envioValue.textContent = costoEnvio.toFixed(2);
+            totalValue.textContent = (subtotal + costoEnvio).toFixed(2);
+        });
+}
+
+radioButtons.forEach((radio) => {
+    radio.addEventListener('change', recalcularCostos);
+});
+
+carrito.forEach((element) => {
+    const cantidadInput = document.getElementById(`cantidad_${element.id}`);
+    cantidadInput.addEventListener('input', recalcularCostos);
+});
 
 
 let Tarjeta_credito = document.getElementById("credito");
